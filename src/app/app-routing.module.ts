2b5296cb912a953499ccc81bfb45d2a111dafa56
Spencer A Warren <spencer.warren.19@cnu.edu>
2023-03-21 16:19:23 -0400

    Tasks Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'tasks', component: TasksComponent }
 
 ];
 
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class TasksComponent {
 
+  constructor() { }
+  fakeTasks = [
+    {
+      id: 1,
+      title: 'Task 1',
+      description: 'Description 1',
+      status: 'Done',
+      dueDate: '2021-05-01'
+    },
+    {
+      id: 2,
+      title: 'Task 2',
+      description: 'Description 2',
+      status: 'In Progress',
+      dueDate: '2021-05-02'
+    },
+    {
+      id: 3,
+      title: 'Task 3',
+      description: 'Description 3',
+      status: 'In Progress',
+      dueDate: '2021-05-03'
+    },
+    {
+      id: 4,
+      title: 'Task 4',
+      description: 'Description 4',
+      status: 'In Progress',
+      dueDate: '2021-05-04'
+    }
+  ];
 }
